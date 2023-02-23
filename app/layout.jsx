af4f125ecb62000a89741d4d06e5224794ffe7fd
Elber Domingos <elberrd@gmail.com>
2023-02-23 17:39:11 -0500

    AddPost changes

--- app/layout.jsx
+++ app/layout.jsx
@@ -2,6 +2,8 @@ import "./globals.css";
 import Nav from "./auth/Nav";
 import { Roboto } from "@next/font/google";
 
+import QueryWrapper from "./auth/QueryWrapper";
+
 const roboto = Roboto({
   subsets: ["latin"],
   weight: ["400", "700"],
@@ -17,8 +19,10 @@ export default function RootLayout({ children }) {
       */}
       <head />
       <body className={`mx-4 md:mx-48 xl:mx-96 ${roboto.variable} bg-gray-200`}>
-        <Nav />
-        {children}
+        <QueryWrapper>
+          <Nav />
+          {children}
+        </QueryWrapper>
       </body>
     </html>
   );
