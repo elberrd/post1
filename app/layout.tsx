ea88e5d5a52b75d7bf418698130c2dcfc004eb8b
Elber Domingos <elberrd@gmail.com>
2023-02-22 22:56:44 -0500

    testing colors

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,12 @@
 import "./globals.css";
 import Nav from "./auth/Nav";
+import { Roboto } from "@next/font/google";
+
+const roboto = Roboto({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+  variable: "--font-roboto",
+});
 
 export default function RootLayout({
   children,
@@ -13,7 +20,7 @@ export default function RootLayout({
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
-      <body>
+      <body className={`mx-4 md:mx-48 xl:mx-96 ${roboto.variable} bg-gray-200`}>
         <Nav />
         {children}
       </body>
